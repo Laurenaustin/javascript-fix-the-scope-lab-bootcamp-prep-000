0a6f31ea13cd6b9e33cdbc3a3e315a67b9267e95
Lauren Schwartz <lschwartzwriting@gmail.com>
2017-12-08 20:37:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,11 +8,9 @@ function yourAnimal(animal) {
   return animal
 }
 
+const two = 2
 function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
-  const two = 2
+  return (n + two)
 }
 
 var funkyFunction = function() {
